31bf8a152cbf80037603a934d470e6727e64a819
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-07 14:12:29 +0300

    ui: Created simple header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { db } from "@/db";
 export default async function Home() {
-  const results = await db.query.testing.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {results.map((result: any) => (
-          <h1 key={result.id}>{result.name}</h1>
-        ))}
-      </div>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
