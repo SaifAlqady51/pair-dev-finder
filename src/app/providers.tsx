110ad8514936647b1054b66f0c384ef077ede0a2
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-07 14:11:31 +0300

    refactor: Created provider wrapper

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  );
+}
