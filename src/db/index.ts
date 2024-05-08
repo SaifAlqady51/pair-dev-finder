535db5d9f65a80a19093837399172d3713387091
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-08 12:30:48 +0300

    refacotr: Changed db and migrate code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,14 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/node-postgres";
 import postgres from "postgres";
 import * as schema from "./schema";
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 // for query purposes
 const queryClient = postgres(process.env.DATABASE_URL!);
-const db = drizzle(queryClient, { schema });
+const db = drizzle(pool);
 
 export { db };
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,4 +1,16 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import {db} from './index'
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
-migrate(db,{migrationsFolder:"drizzle"})
+async function main(){
+
+    console.log("migration starts")
+    await migrate(db ,{migrationsFolder:"drizzle"})
+    console.log("migration ends")
+    process.exit()
+}
+
+main().catch(err => {
+    console.log(err);
+    process.exit()
+})
