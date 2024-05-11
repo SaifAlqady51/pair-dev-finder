b655c0fab59fc50790def177bb42e9dd8c6b0aff
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-11 18:14:54 +0300

    feat: Added github provider to next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,8 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { AuthOptions, getServerSession } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+
 
 export const authConfig = {
 
@@ -11,6 +13,10 @@ export const authConfig = {
         strategy:'jwt'
     },
     providers: [
+        GithubProvider({
+          clientId:process.env.GITHUB_CLIENT_ID!,
+          clientSecret:process.env.GITHUB_CLIENT_SECRET!
+        }),
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID!,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET!
