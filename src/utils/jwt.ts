bb5125b0a8a6edc7878f234e796ab6bc3bd7e727
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-12 17:37:24 +0300

    feat: Created encrypt & decrypt functions for jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,13 @@
+import * as jwt from "jsonwebtoken";
+
+
+export function encrypt(message:object){
+        return jwt.sign(message, process.env.JWT_SECRET!, {
+        algorithm: "HS512",
+        })
+}
+
+export function decrypt(token:string){
+
+  return jwt.decode(token);
+}
