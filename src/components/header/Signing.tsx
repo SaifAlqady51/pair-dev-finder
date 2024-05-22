39c296ccdfef66750771dc34fc1982af042e27ab
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-22 10:55:31 +0300

    styles: Chagned login register buttons styles

--- src/components/header/Signing.tsx
+++ src/components/header/Signing.tsx
@@ -4,12 +4,16 @@ import Link from "next/link";
 export default function Signing() {
   return (
     <div className="flex justify-between items-center gap-4">
-      <Button>
-        <Link href="/signing/login">Log In</Link>
+      <Button variant="ghost">
+        <Link href="/signing/login" className="text-lg font-medium">
+          Log In
+        </Link>
       </Button>
 
-      <Button>
-        <Link href="/signing/email-check">Register</Link>
+      <Button variant="ghost">
+        <Link href="/signing/email-check" className="text-lg font-medium">
+          Register
+        </Link>
       </Button>
     </div>
   );
