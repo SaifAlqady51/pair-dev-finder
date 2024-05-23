81f035529a021b9888e82627b6f6c902e6a33d02
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-23 11:53:04 +0300

    styles: Chagne cards styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,18 @@ export default async function Home() {
   const rooms = await getRooms();
 
   return (
-    <main className="min-h-screen   p-24">
+    <main className="min-h-screen pt-32 px-16">
       <div className="flex justify-between items-center w-full mb-12">
         <h2 className="text-4xl font-medium">Find Dev Rooms</h2>
         <Button className="font-semibold">
           <Link href="/create-room">Create Room</Link>
         </Button>
       </div>
-      {rooms.map((room: Room) => (
-        <RoomCard room={room} key={room.id} />
-      ))}
+      <div className="grid grid-cols-3 gap-6">
+        {rooms.map((room: Room) => (
+          <RoomCard room={room} key={room.id} />
+        ))}
+      </div>
     </main>
   );
 }
