8c181173d6d1c168910f16b2fe9c4dac448a61da
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-23 11:45:43 +0300

    env: Added GITHUB_TOKEN to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
     env:{
         JWT_SECRET : process.env.JWT_SECRET,
-        SALT_ROUNDS : process.env.SALT_ROUNDS
+        SALT_ROUNDS : process.env.SALT_ROUNDS,
+        GITHUB_TOKEN : process.env.GITHUB_TOKEN
     }
 };
 
