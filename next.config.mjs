ba0d076baada0963f1fe3efa607692fab4eec055
SaifAlqady51 <saifalqady51@gmail.com>
2024-05-28 20:16:48 +0300

    refactor: Added env vars to next config gile

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
     env:{
         JWT_SECRET : process.env.JWT_SECRET,
         SALT_ROUNDS : process.env.SALT_ROUNDS,
-        GITHUB_TOKEN : process.env.GITHUB_TOKEN
+        GITHUB_TOKEN : process.env.GITHUB_TOKEN,
+        PUSHER_KEY: process.env.PUSHER_KEY,
+        PUSHER_APP_ID:process.env.PUSHER_APP_ID,
+        PUSHER_SECRET:process.env.PUSHER_SECRET
     }
 };
 
