d04c6a5661ba4372403171732ba39d6b2fb5acb1
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-14 20:04:41 +0300

    feat: Created roomCard story file

---
+++ src/stories/roomCard.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from "@storybook/react";
+import RoomCard from "@/components/RoomCard";
+import { Room } from "@/db/schema";
+
+const meta: Meta<typeof RoomCard> = {
+  title: "Components/RoomCard",
+  component: RoomCard,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RoomCard>;
+
+const room: Room = {
+  id: "1",
+  name: "Example Room",
+  description: "This is an example room.",
+  tags: "example,story,room",
+  githubRepo: "https://github.com/example/repo",
+  userId: "user123",
+  created_at: new Date(),
+};
+
+export const Default: Story = {
+  args: {
+    room,
+  },
+};
