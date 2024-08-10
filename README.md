7df22954c79dee08a9c05ea543762e430803b9ce
SaifAlqady51 <saifalqady51@gmail.com>
2024-08-10 17:47:18 +0300

    readme: Edited Readme file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pair dev finder
 
 Connect, Create, and Excel with Fellow Developers
+
+## Tech Used
+
+- Nextjs
+- Drizzle
+- Postgresql
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,10 @@
 import * as jwt from "jsonwebtoken";
 
-
 export function encrypt(message:object){
     return jwt.sign(message, process.env.JWT_SECRET!, {
         algorithm: "HS512",
     })
 }
-
 export function decrypt(token:string){
-
   return jwt.decode(token);
 }
