6422ccf1611f61ac5c24e8f7fa8c119e2379690c
SaifAlqady51 <saifalqady51@gmail.com>
2025-01-11 18:41:56 +0200

    refactor: Renamed FullREgisterForm to CreateAccountForm

--- src/app/authentication/register/create-account/page.tsx
+++ src/app/authentication/register/create-account/page.tsx
@@ -1,4 +1,4 @@
-import { FullRegisterForm } from "@/components/forms/FullRegisterForm";
+import { CreateAccountForm } from "@/components/forms/CreateAccountForm";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
@@ -10,7 +10,7 @@ export default function CodePage() {
   return (
     <Suspense>
       <h1 className="font-semibold text-3xl">Continue your registration</h1>
-      <FullRegisterForm />
+      <CreateAccountForm />
     </Suspense>
   );
 }
