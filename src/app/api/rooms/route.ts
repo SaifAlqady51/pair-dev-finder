427f4865191a9e3a8a347d19960ffe4593e8415c
SaifAlqady51 <saifalqady51@gmail.com>
2025-01-31 14:37:29 +0200

    clear: Clear up consoles

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -25,23 +25,19 @@ export async function GET() {
 export async function POST(req: Request) {
   try {
     const data = await req.json();
-    console.log(data);
 
     const session = await getSession();
     if (!session) {
       throw new Error("You have to sign in first");
     }
-    console.log("USERID  " + session.user.id);
 
     const insertedRoom = await db
       .insert(rooms)
       .values({ ...data, userId: session.user.id });
 
     revalidatePath("/");
-    console.log("INsertedRoom  " + insertedRoom);
     return NextResponse.json({ success: true, data: insertedRoom });
   } catch (error) {
-    console.log("Errorrrrrrrrrrrrrrrrrrrrrrrr");
     return NextResponse.json(
       { success: false, message: "Error creating room." + error },
       { status: 500 },
