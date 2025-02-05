f34eb0900a3fa1bf971020e6adb03ba798bb74ff
SaifAlqady51 <saifalqady51@gmail.com>
2025-02-05 15:50:23 +0200

    feat: Adding index.ts to schemas folder

--- src/schemas/loginFormSchema.ts
+++ src/schemas/loginFormSchema.ts
@@ -4,3 +4,4 @@ export const loginFormSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string(),
 });
+export type LoginUserType = { email: string; password: string };
