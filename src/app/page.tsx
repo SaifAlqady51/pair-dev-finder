a2da8fc315a36fd9d25428781bb62f646f718d3d
SaifAlqady51 <saifalqady51@gmail.com>
2025-02-12 13:21:35 +0200

    ui: Updated main page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,38 +16,38 @@ export default async function Home() {
   }
 
   return (
-    <main className="min-h-screen pt-32 px-16">
-      <div className="flex md:flex-row gap-6 flex-col justify-between items-center w-full mb-12">
-        <h2 className="md:text-4xl text-3xl font-bold" data-cy="find-dev-title">
-          Find Dev Rooms
-        </h2>
-        <Button className="font-semibold md:text-xl text-2xl md:p-6 px-10 py-6">
-          <Link href="/create-room">Create Room</Link>
-        </Button>
-      </div>
+    <main className=" w-full min-h-screen flex justify-center items-center ">
+      <div className="min-h-screen md:w-10/12 w-full pt-32 px-16">
+        <div className="flex md:flex-row gap-6 flex-col justify-between items-center w-full mb-12">
+          <h2
+            className="md:text-4xl text-3xl font-bold"
+            data-cy="find-dev-title"
+          >
+            Find Dev Rooms
+          </h2>
+          <Button className="font-semibold md:text-xl text-2xl md:p-6 px-10 py-6">
+            <Link href="/create-room">Create Room</Link>
+          </Button>
+        </div>
 
-      <div className="flex justify-center items-center min-h-[30vh]">
-        {error ? (
-          <p
-            className="text-2xl font-light text-red-500"
-            data-cy="error-message"
-          >
-            {error}
-          </p>
-        ) : rooms.length > 0 ? (
-          <div
-            className="w-full grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6"
-            data-cy="room-list"
-          >
-            {rooms.map((room) => (
-              <RoomCard room={room} key={room.id} />
-            ))}
-          </div>
-        ) : (
-          <p className="text-2xl font-light" data-cy="no-rooms-message">
-            No rooms available
-          </p>
-        )}
+        <div className="flex justify-center items-center min-h-[30vh]">
+          {error ? (
+            <p className="text-2xl font-light text-red-500">{error}</p>
+          ) : rooms.length > 0 ? (
+            <div
+              className=" w-full grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6"
+              data-cy="room-list"
+            >
+              {rooms.map((room) => (
+                <RoomCard room={room} key={room.id} />
+              ))}
+            </div>
+          ) : (
+            <p className="text-2xl font-light" data-cy="no-rooms-message">
+              No rooms available
+            </p>
+          )}
+        </div>
       </div>
     </main>
   );
