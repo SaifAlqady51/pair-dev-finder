c06f0f4a60ced577a5f2d08d8a2ffcacfb72f1cd
SaifAlqady51 <saifalqady51@gmail.com>
2025-02-16 15:50:57 +0200

    refactor: Edited roomFormFieldsData types

--- src/data/roomFormFieldsData.ts
+++ src/data/roomFormFieldsData.ts
@@ -1,7 +1,8 @@
 export type RoomFormFieldDataType = {
-  fieldName: "name" | "tags" | "description" | "githubRepo" | "image";
+  fieldName: "name" | "keywords" | "description" | "githubRepo" | "image";
   label: string;
   placeholder?: string;
+  "data-cy"?: string;
 };
 
 // This is just a labels data to display on ui
@@ -10,25 +11,30 @@ export const roomFormFieldsData: RoomFormFieldDataType[] = [
     fieldName: "name",
     label: "name",
     placeholder: "John Doe",
+    "data-cy": "create-room-form-name-field",
   },
   {
     fieldName: "description",
     placeholder: "Talk about your room purpose, goals, etc.",
     label: "description",
+    "data-cy": "create-room-form-description-field",
   },
   {
-    fieldName: "tags",
-    label: "Tags",
+    fieldName: "keywords",
+    label: "Keywords",
     placeholder: "Technologies used (e.g., TypeScript, React)",
+    "data-cy": "create-room-form-tags-field",
   },
   {
     fieldName: "githubRepo",
     label: "github repo",
     placeholder: "https:/github.com/user/repo",
+    "data-cy": "create-room-form-github-link-field",
   },
   {
     fieldName: "image",
     label: "Room Image",
     placeholder: "https://image.com/image.jpg",
+    "data-cy": "create-room-form-image-field",
   },
 ];
