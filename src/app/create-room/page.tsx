2224bd235c0f01d2b999149b4eea27e7ca75ebf7
SaifAlqady51 <saifalqady51@gmail.com>
2025-02-17 19:03:56 +0200

    refactor: Removed login header from LoginForm to login/page

--- src/app/create-room/page.tsx
+++ src/app/create-room/page.tsx
@@ -3,7 +3,9 @@ import { CreateRoomForm } from "@/components/CreateRoomForm";
 export default function CreateRoomPage() {
   return (
     <div className=" container mx-auto flex flex-col gap-8 mt-24 max-w-[800px] ">
-      <h1 className="text-4xl font-semibold ">Create Room</h1>
+      <h1 className="text-4xl font-semibold" data-cy="create-room-header">
+        Create a Room
+      </h1>
       <CreateRoomForm />
     </div>
   );
