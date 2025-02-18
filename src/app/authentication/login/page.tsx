e101e8a29b2443c69e65a9bd6708c37eafbd6d69
SaifAlqady51 <saifalqady51@gmail.com>
2025-02-18 12:19:42 +0200

    refactor: Added Login header in login page instead of LoginForm

--- src/app/authentication/login/page.tsx
+++ src/app/authentication/login/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LoginForm } from "@/components/forms/LoginFrom";
 import { ProviderButtons } from "@/components/forms/ProvidersButtons";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,6 +10,21 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <React.Fragment>
+      <div className="space-y-3">
+        <h1 className="font-semibold text-[1.8rem] md:text-3xl ">
+          Log in to your account
+        </h1>
+        <div className="flex gap-1 text-gray-300 text-sm">
+          <p>{"Don't have an account?"}</p>
+          <Link
+            href="/authentication/register/verify-email/"
+            className="text-blue-500"
+            data-cy="register-link" // Add this line
+          >
+            Register
+          </Link>
+        </div>
+      </div>
       <LoginForm />
       <ProviderButtons />
     </React.Fragment>
