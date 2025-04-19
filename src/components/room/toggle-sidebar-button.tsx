182a4b3b7ea93149e0f02d2914e5d0e1faf78107
SaifAlqady51 <saifalqady51@gmail.com>
2025-04-19 13:38:00 +0200

    refactor: Edited ToggleSidebarButton logic

--- src/components/room/toggle-sidebar-button.tsx
+++ src/components/room/toggle-sidebar-button.tsx
@@ -1,15 +1,19 @@
 import { HiDotsHorizontal } from "react-icons/hi";
 import { Indicator } from "../ui/indicator";
 import { SidebarTrigger } from "../ui/sidebar";
-import { useUnreadMessagesCounter } from "../providers";
 
-export const ToggleSidebarButton = () => {
-  const { unreadMessagesCount } = useUnreadMessagesCounter();
+interface ToggleSidebarButtonProps {
+  unreadMessagesCount: number;
+}
+
+export const ToggleSidebarButton: React.FC<ToggleSidebarButtonProps> = ({
+  unreadMessagesCount,
+}) => {
   return (
     <Indicator
       count={unreadMessagesCount}
       position="top-right"
-      badgeClassName="h-6 w-6 text-[10px] bg-blue-500"
+      badgeClassName="h-6 w-6 text-xs font-medium bg-blue-500"
     >
       <SidebarTrigger className="flex w-11 h-11 bg-slate-900 hover:bg-slate-800 dark-slate-400 rounded-md p-2 opacity-70">
         <HiDotsHorizontal className="text-white w-7 h-7" />
