4ddd2710f8aaf4653449b612f769871563ee7b31
SaifAlqady51 <saifalqady51@gmail.com>
2025-04-20 10:37:32 +0200

    config: Added vars to env

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env:{
-        JWT_SECRET : process.env.JWT_SECRET,
-        SALT_ROUNDS : process.env.SALT_ROUNDS,
-        GITHUB_TOKEN : process.env.GITHUB_TOKEN,
-        PUSHER_KEY: process.env.PUSHER_KEY,
-        PUSHER_APP_ID:process.env.PUSHER_APP_ID,
-        PUSHER_SECRET:process.env.PUSHER_SECRET
-    }
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    SALT_ROUNDS: process.env.SALT_ROUNDS,
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_SECRET: process.env.PUSHER_SECRET,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+  },
 };
 
 export default nextConfig;
