5a61947e71e1ff6c53e461f12ebcfc1ad3129893
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-07 22:15:19 +0300

    refactor: Edited create-room page

--- src/app/create-room/page.tsx
+++ src/app/create-room/page.tsx
@@ -2,7 +2,7 @@ import { CreateRoomForm } from "@/components";
 
 export default function CreateRoomPage() {
   return (
-    <div className=" container mx-auto flex flex-col gap-8 mt-24 max-w-[800px] ">
+    <div className="container  flex flex-col gap-8 mt-24 max-w-[900px]">
       <h1 className="text-4xl font-semibold" data-cy="create-room-header">
         Create a Room
       </h1>
